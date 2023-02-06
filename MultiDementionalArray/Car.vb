56236110cbba5c6b2887ea52b63b6d841d4df0e3
Rekhakrish <rekhabca98@gmail.com>
2023-02-06 17:01:56 +0530

    Add project files.

---
+++ MultiDementionalArray/Car.vb
@@ -0,0 +1,23 @@
+﻿Public Class Car
+
+    'Member Variables
+    Public engine As String
+    Public noOfWheels As Integer
+
+
+    'Member Functions
+    Public Sub AcceptDetails()
+
+        Console.WriteLine("Enter the Engine MOdel")
+        engine = Console.ReadLine()
+        Console.WriteLine("Enter the Number of wheels")
+        noOfWheels = Console.ReadLine()
+    End Sub
+    Public Sub DisplayDetails()
+        Console.WriteLine("The Engine model: {0}", engine)
+        Console.WriteLine("The number of Wheels are: {0}", noOfWheels)
+
+
+    End Sub
+
+End Class
---
+++ MultiDementionalArray/Operator.vb
@@ -0,0 +1,60 @@
+﻿Public Class Operators
+    Dim a As Int32
+    Dim b As Int32
+    Dim c As Int32
+    Public Sub ArthemeticOperations()
+        Console.WriteLine("Enter the num1: ")
+        a = Convert.ToInt32(Console.ReadLine())
+        Console.WriteLine("Enter the num2: ")
+        b = Convert.ToInt32(Console.ReadLine())
+        c = a + b
+        Console.WriteLine("After add the result is: {0}", c)
+        Console.WriteLine("Enter the num1: ")
+        a = Convert.ToInt32(Console.ReadLine())
+        Console.WriteLine("Enter the num2: ")
+        b = Convert.ToInt32(Console.ReadLine())
+        c = a - b
+        Console.WriteLine("After sub the result is: {0}", c)
+        Console.WriteLine("Enter the num1: ")
+        a = Convert.ToInt32(Console.ReadLine())
+        Console.WriteLine("Enter the num2: ")
+        b = Convert.ToInt32(Console.ReadLine())
+        c = a * b
+        Console.WriteLine("After multiply the result is: {0}", c)
+        Console.WriteLine("Enter the num1: ")
+        a = Convert.ToInt32(Console.ReadLine())
+        Console.WriteLine("Enter the num2: ")
+        b = Convert.ToInt32(Console.ReadLine())
+        c = a / b
+        Console.WriteLine("After Divide the result is: {0}", c)
+        Console.WriteLine("Enter the num1: ")
+        a = Convert.ToInt32(Console.ReadLine())
+        Console.WriteLine("Enter the num2: ")
+        b = Convert.ToInt32(Console.ReadLine())
+        c = a Mod b
+        Console.WriteLine("After Mod the result is: {0}", c)
+    End Sub
+    Public Sub AllOperations()
+        Dim Number1 As Integer
+        Dim Number2 As Integer
+        Dim Result As Integer
+        'Dim Unary As Integer
+
+        Console.WriteLine("Enter the Number1: ")
+        Number1 = Console.ReadLine()
+        Console.WriteLine("Enter the Number2: ")
+        Number2 = Console.ReadLine()
+
+        'Arithmetic Opearator
+        Result = Number1 + Number2
+        Console.WriteLine("Result : " & Result)
+
+        If Result = 10 Then
+            Console.WriteLine("Get the Numberb")
+        ElseIf (Result > 10) Then
+            Console.WriteLine("Greater than Number")
+        Else
+            Console.WriteLine("Below the Number")
+        End If
+    End Sub
+End Class
